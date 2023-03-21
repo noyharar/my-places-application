111b1a8b5d5d014253c6ac8eedb2c8d966a001d7
noyharar <noyharar@post.bgu.ac.il>
2023-03-21 11:47:57 +0200

    Auth - login and signup

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import NewPlace from './places/pages/NewPlace';
 import UserPlaces from './places/pages/UserPlaces';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 import UpdatePlace from "./places/pages/UpdatePlace";
+import Auth from "./user/pages/Auth";
 
 const App = () => {
   return (
@@ -30,6 +31,9 @@ const App = () => {
             <Route path="/places/:placeId" exact>
               <UpdatePlace />
             </Route>
+          <Route path="/auth" exact>
+            <Auth />
+          </Route>
             <Redirect to="/" />
           </Switch>
         </main>
